79f1bbe6d0873c9dc9c302b3b44e710f0f2fbb92
Michael Quinn <michael@cherrysoft.com>
2020-11-17 12:59:12 +0100

    making tmp dir and moving docker files temporarily due to laravel installer having getting borked for non empty dirs

--- generators/app/templates/build.sh
+++ generators/app/templates/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 app=${PWD##*/}_app
 
-laravel new
+mkdir /tmp/${app}
+mv * /tmp/${app}
+
+composer create-project --prefer-dist laravel/laravel .
+
+cp /tmp/${app}/* .
 
 docker-compose build && docker-compose up -d && docker exec -it $app /bin/bash
